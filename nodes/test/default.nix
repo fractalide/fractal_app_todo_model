4fa2b30ba9cde1392adacb3fa0512cc938441818
Stewart Mackenzie <setori88@gmail.com>
2017-01-05 00:15:25 +0800

    Problem: Schema in fractals will probably name collide
    Solution: schema must have full qualified class name and dir hierarchy

--- nodes/test/default.nix
+++ nodes/test/default.nix
@@ -2,16 +2,16 @@
 
 subgraph {
   src = ./.;
-  edges = with edges; [ path generic_text ];
+  edges = with edges; [ fs_path prim_text ];
   flowscript = with nodes; with edges; ''
-  '${path}:(path="${builtins.getEnv "HOME"}/todos.db")' -> db_path model(${model})
-  '${generic_text}:(text="tcp://127.0.0.1:5551")' -> request_get model()
-  '${generic_text}:(text="tcp://127.0.0.1:5552")' -> request_post model()
-  '${generic_text}:(text="tcp://127.0.0.1:5553")' -> request_delete model()
-  '${generic_text}:(text="tcp://127.0.0.1:5554")' -> request_patch model()
-  '${generic_text}:(text="tcp://127.0.0.1:5555")' -> response_get model()
-  '${generic_text}:(text="tcp://127.0.0.1:5556")' -> response_post model()
-  '${generic_text}:(text="tcp://127.0.0.1:5557")' -> response_delete model()
-  '${generic_text}:(text="tcp://127.0.0.1:5558")' -> response_patch model()
+  '${fs_path}:(path="${builtins.getEnv "HOME"}/todos.db")' -> db_path model(${model})
+  '${prim_text}:(text="tcp://127.0.0.1:5551")' -> request_get model()
+  '${prim_text}:(text="tcp://127.0.0.1:5552")' -> request_post model()
+  '${prim_text}:(text="tcp://127.0.0.1:5553")' -> request_delete model()
+  '${prim_text}:(text="tcp://127.0.0.1:5554")' -> request_patch model()
+  '${prim_text}:(text="tcp://127.0.0.1:5555")' -> response_get model()
+  '${prim_text}:(text="tcp://127.0.0.1:5556")' -> response_post model()
+  '${prim_text}:(text="tcp://127.0.0.1:5557")' -> response_delete model()
+  '${prim_text}:(text="tcp://127.0.0.1:5558")' -> response_patch model()
   '';
 }
