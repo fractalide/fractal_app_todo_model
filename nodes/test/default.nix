89592d1d7018905865fbe501dda814d95ca86c73
Stewart Mackenzie <setori88@gmail.com>
2017-02-09 21:31:51 +0800

    Problem: capnp compilation process must be fbp agnostic
    Solution: the product of capnp encode should be the imsg
    input. capnp tool shouldn't be called from rust code because
    that means the fbp graph implementation is dependent on capnp at
    run time.
    
    By making the fbp graph implementation dependent on the product
    of the capnp encode output the graph becomes portable. i.e.
    subgraphs can be parsed in javascript and the implemantation
    doesn't have to depend on capnp tool at runtime... in the
    browser this is problematic.

--- nodes/test/default.nix
+++ nodes/test/default.nix
@@ -1,19 +1,26 @@
-{ subgraph, imsgs, nodes, edges }:
-
-subgraph rec {
+{ subgraph, imsg, nodes, edges }:
+let
+  FsPath = imsg { class = edges.FsPath; text = ''(path="${builtins.getEnv "HOME"}/todos.db")''; };
+  PrimText1 = imsg { class = edges.PrimText; text = ''(text="tcp://127.0.0.1:5551")''; };
+  PrimText2 = imsg { class = edges.PrimText; text = ''(text="tcp://127.0.0.1:5552")''; };
+  PrimText3 = imsg { class = edges.PrimText; text = ''(text="tcp://127.0.0.1:5553")''; };
+  PrimText4 = imsg { class = edges.PrimText; text = ''(text="tcp://127.0.0.1:5554")''; };
+  PrimText5 = imsg { class = edges.PrimText; text = ''(text="tcp://127.0.0.1:5555")''; };
+  PrimText6 = imsg { class = edges.PrimText; text = ''(text="tcp://127.0.0.1:5556")''; };
+  PrimText7 = imsg { class = edges.PrimText; text = ''(text="tcp://127.0.0.1:5557")''; };
+  PrimText8 = imsg { class = edges.PrimText; text = ''(text="tcp://127.0.0.1:5558")''; };
+in
+subgraph {
   src = ./.;
-  imsg = imsgs {
-    edges = with edges; [ FsPath PrimText];
-  };
   flowscript = with nodes; ''
-  '${imsg}.FsPath:(path="${builtins.getEnv "HOME"}/todos.db")' -> db_path model(${model})
-  '${imsg}.PrimText:(text="tcp://127.0.0.1:5551")' -> request_get model()
-  '${imsg}.PrimText:(text="tcp://127.0.0.1:5552")' -> request_post model()
-  '${imsg}.PrimText:(text="tcp://127.0.0.1:5553")' -> request_delete model()
-  '${imsg}.PrimText:(text="tcp://127.0.0.1:5554")' -> request_patch model()
-  '${imsg}.PrimText:(text="tcp://127.0.0.1:5555")' -> response_get model()
-  '${imsg}.PrimText:(text="tcp://127.0.0.1:5556")' -> response_post model()
-  '${imsg}.PrimText:(text="tcp://127.0.0.1:5557")' -> response_delete model()
-  '${imsg}.PrimText:(text="tcp://127.0.0.1:5558")' -> response_patch model()
+    '${FsPath}' -> db_path model(${model})
+    '${PrimText1}' -> request_get model()
+    '${PrimText2}' -> request_post model()
+    '${PrimText3}' -> request_delete model()
+    '${PrimText4}' -> request_patch model()
+    '${PrimText5}' -> response_get model()
+    '${PrimText6}' -> response_post model()
+    '${PrimText7}' -> response_delete model()
+    '${PrimText8}' -> response_patch model()
   '';
 }
